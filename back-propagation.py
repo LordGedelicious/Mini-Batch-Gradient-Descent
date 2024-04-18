46da84d7aea0a8886e91acad96c6fb0e2b130983
Naufal-Nalendra <16521240@std.stei.itb.ac.id>
2024-04-18 20:31:37 +0700

    Update back-propagation.py

--- back-propagation.py
+++ back-propagation.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 def error (output, target):
     return 0.5 * (target - output) ** 2
@@ -10,3 +11,44 @@ def dNet_dW(inputs, index):
 
 def back_propagation(model_src_name):
     pass
+
+# Activation functions
+def linear(arr):
+    return arr
+
+def relu(val):
+    if val < 0:
+        return 0
+    else:
+        return val
+    
+def sigmoid(arr):
+    numerator = np.exp(arr)
+    denominator = numerator + 1
+    return numerator / denominator
+
+def softmax(arr):
+    arr = np.array(arr)
+    arr = np.exp(arr)
+    sum_arr = np.sum(arr)
+    return arr/sum_arr
+
+# Derivative versions for activation functions
+# Derivative of linear activation function is simply 1
+def linear_derivative(arr):
+    return np.ones_like(arr)
+
+# Derivative of ReLU activation function
+def relu_derivative(arr):
+    return np.where(arr > 0, 1, 0)
+
+# Derivative of sigmoid activation function
+def sigmoid_derivative(arr):
+    sigmoid_output = sigmoid(arr)
+    return sigmoid_output * (1 - sigmoid_output)
+
+# Derivative of softmax activation function
+def softmax_derivative(arr):
+    softmax_output = softmax(arr)
+    jacobian_matrix = np.diag(softmax_output) - np.outer(softmax_output, softmax_output)
+    return jacobian_matrix
